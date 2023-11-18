d3b35c4cf49b63e3410a3268030039d30fcd9a1f
Shahram Shakiba <ShahramShakiba88@gmail.com>
2023-11-18 17:17:26 +0330

    Add functionality of adding Todo-list to  TaskBox

---
+++ index.js
@@ -0,0 +1,98 @@
+const taskInput = document.querySelector('.task-input input'),
+  filters = document.querySelectorAll('.filters span'),
+  clearAll = document.querySelector('.clear-btn'),
+  taskBox = document.querySelector('.task-box');
+
+let editId;
+isEditTask = false;
+
+todos = JSON.parse(localStorage.getItem('todo-list'));
+
+//==>> Filter options functionality
+filters.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    document.querySelector('span.active').classList.remove('active');
+
+    btn.classList.add('active');
+
+    showTodo(btn.id);
+  });
+});
+
+//=> Show Todo List
+const showTodo = (filter) => {
+  let todoList = '';
+
+  if (todos) {
+    todos.forEach((todo, id) => {
+      let completed = (todo.status = 'completed' ? 'checked' : '');
+
+      if (filter == todo.status || filter == 'all') {
+        todoList += `
+        <li class="task">
+          <label for="${id}">
+            <input onclick="updateStatus(this)" type="checkbox" id="${id}" ${completed}>
+            
+            <p class="${completed}">
+              ${todo.name}
+            </p>
+          </label>
+
+          <div class="settings">
+            <i onclick="showMenu(this)" class="uil uil-ellipsis-h"></i>
+            
+            <ul class="task-menu">
+              <li onclick='editTask(${id}, "${todo.name}")'>
+                <i class="uil uil-pen"></i>
+                Edit
+              </li>
+
+              <li onclick='deleteTask(${id}, "${filter}")'>
+                <i class="uil uil-trash"></i>
+                Delete
+              </li>
+            </ul>
+          </div>
+        </li>
+        `;
+      }
+    });
+  }
+
+  taskBox.innerHTML = todoList || `<span>You don't have any task here</span>`;
+
+  let checkTask = taskBox.querySelectorAll('.task');
+
+  !checkTask.length
+    ? clearAll.classList.remove('active')
+    : clearAll.classList.add('active');
+
+  taskBox.offsetHeight >= 300
+    ? taskBox.classList.add('overflow')
+    : taskBox.classList.remove('overflow');
+};
+
+showTodo('all');
+
+//==>> Add Todo to task-box
+taskInput.addEventListener('keyup', (e) => {
+  let userTask = taskInput.value.trim();
+
+  if (e.key === 'Enter' && userTask) {
+    if (!isEditTask) {
+      todos = !todos ? [] : todos;
+
+      let taskInfo = { name: userTask, status: 'pending' };
+
+      todos.push(taskInfo);
+    } else {
+      isEditTask = false;
+      todos[editId].name = userTask;
+    }
+
+    taskInput.value = '';
+    localStorage.setItem('todo-list', JSON.stringify(todos));
+
+    showTodo(document.querySelector('span.active').id);
+  }
+});
